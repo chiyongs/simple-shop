e7128081a2fa218f11ce2c00c92b14cabdb47088
chiyongs <tlscldyd@gmail.com>
2022-06-05 16:41:11 +0900

    feat : Promotion 삭제 API 추가 및 존재하지 않는 프로모션 에러코드 추가

--- src/main/java/com/ssg/techrookie/backend/exception/ErrorCode.java
+++ src/main/java/com/ssg/techrookie/backend/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
     NOT_SUPPORTED_HTTP_METHOD(HttpStatus.BAD_REQUEST,"지원하지 않는 Http Method 방식입니다."),
     NOT_VALID_METHOD_ARGUMENT(HttpStatus.BAD_REQUEST,"유효하지 않은 Request Body 혹은 Argument입니다."),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다."),
-    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않은 상품입니다.");
+    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다."),
+    PROMOTION_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 프로모션입니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/ssg/techrookie/backend/web/controller/PromotionApiController.java
+++ src/main/java/com/ssg/techrookie/backend/web/controller/PromotionApiController.java
@@ -5,10 +5,7 @@ import com.ssg.techrookie.backend.web.dto.ApiResponse;
 import com.ssg.techrookie.backend.web.dto.promotion.PromotionSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/promotions")
@@ -20,5 +17,11 @@ public class PromotionApiController {
     @PostMapping
     public ApiResponse<Long> promotionAdd(@Validated @RequestBody PromotionSaveRequestDto requestDto) {
         return ApiResponse.ok(promotionService.addPromotion(requestDto));
+    }
+
+    @DeleteMapping
+    public ApiResponse<Long> promotionDelete(@Validated @RequestParam Long promotionId) {
+        promotionService.deletePromotion(promotionId);
+        return ApiResponse.ok(promotionId);
     }
 }
