f681d8d4518847274ed2168f914ce6542db79010
chiyongs <tlscldyd@gmail.com>
2022-06-10 23:02:31 +0900

    refactor : ItemApiController 메서드들에 세부 설명 기재

--- src/main/java/com/ssg/techrookie/backend/web/controller/ItemApiController.java
+++ src/main/java/com/ssg/techrookie/backend/web/controller/ItemApiController.java
@@ -18,22 +18,42 @@ public class ItemApiController {
 
     private final ItemService itemService;
 
+    /**
+     * 상품 등록 메서드
+     * @param requestDto (상품 정보를 담고 있는 dto)
+     * @return 등록한 상품의 id
+     */
     @PostMapping
     public ApiResponse<Long> itemAdd(@Validated @RequestBody ItemSaveRequestDto requestDto) {
         return ApiResponse.ok(itemService.addItem(requestDto));
     }
 
+    /**
+     * 상품 id에 해당하는 상품 삭제 메서드
+     * @param itemId
+     * @return 삭제한 상품의 id
+     */
     @DeleteMapping
     public ApiResponse<Long> itemDelete(@Validated @RequestParam Long itemId) {
         itemService.deleteItem(itemId);
         return ApiResponse.ok(itemId);
     }
 
+    /**
+     * 사용자가 구매할 수 있는 상품 정보를 반환하는 메서드
+     * @param userId
+     * @return 구매가능한 상품 정보들을 담은 리스트
+     */
     @GetMapping("/view")
     public ApiResponse<List<ItemResponseDto>> itemListAvailableForPurchaseByUser(@Validated @RequestParam Long userId) {
         return ApiResponse.ok(itemService.findAvailableForPurchaseByUser(userId));
     }
 
+    /**
+     * 상품 id에 해당하는 상품 정보 반환 메서드
+     * @param itemId
+     * @return 상품 정보를 담고 있는 dto
+     */
     @GetMapping("/{itemId}")
     public ApiResponse<ItemResponseDto> itemDetail(@Validated @PathVariable Long itemId) {
         return ApiResponse.ok(itemService.findById(itemId));
